91bea5679983ed423ed2aad3665e2d566597a360
Martin Malgor <martinmmalgor@gmail.com>
2025-06-12 23:45:51 -0300

    Actualizar la configuración del cliente de Sanity para utilizar variables de entorno en lugar de valores fijos y habilitar el uso de CDN.

--- src/lib/sanityClient.ts
+++ src/lib/sanityClient.ts
@@ -3,10 +3,11 @@ import imageUrlBuilder from '@sanity/image-url';
 import type { SanityImageSource } from '@sanity/image-url/lib/types/types';
 
 const config = {
-    projectId: 'pl01ag7y',
-    dataset: 'production',
+    projectId: import.meta.env.PUBLIC_SANITY_PROJECT_ID,
+    dataset: import.meta.env.PUBLIC_SANITY_DATASET,
     apiVersion: '2024-06-13', // use a UTC date string
-    useCdn: false, // `false` if you want to ensure fresh data
+    useCdn: true, // `true` if you want to use the CDN, `false` if you want to ensure fresh data
+    token: import.meta.env.SANITY_API_READ_TOKEN,
 };
 
 export const sanityClient = createClient(config);
